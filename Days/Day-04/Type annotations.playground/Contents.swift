bc25ba8c2d750a0de0bd4d222ecc399afe33ce45
jeongmu <jeongmuya@gmail.com>
2026-01-25 20:25:32 +0900

    Type annotations and checkpoint 2

---
+++ Days/Day-04/Type annotations.playground/Contents.swift
@@ -0,0 +1,49 @@
+import UIKit
+
+//let surname = "Lasso"
+//var score = 0
+
+
+let surname: String = "Lasso"
+//var score: Int = 0
+
+var score: Double = 0
+
+let palyerName: String = "Roy"
+
+var luckyNumber: Int = 13
+
+let pi: Double = 3.141
+
+var isAuthenticated: Bool = true
+
+var albums: [String] = ["Red", "Fearless"]
+
+var user: [String: String] = ["id": "@twostraws"]
+
+var books: Set<String> = Set(["The Bluset Eye", "Foundation", "The Name of the Wind"])
+
+var soda: [String] = ["Pepsi", "Coke", "Dr Pepper"]
+var teams: [String] = [String]()
+
+var cities: [String] = []
+var clues = [String]()
+
+enum UIStryle {
+    case light, dark, system
+}
+
+var style = UIStryle.light
+
+let username: String
+
+username = "@twostraws"
+
+print(username)
+
+//ler score: Int = "Zero"
+
+let albums2 = ["Red", "Fearless", "The Dark Side of the Moon"]
+
+albums2.count
+Set(albums2)
